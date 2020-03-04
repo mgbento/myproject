c1bc49b8778db7b108874b740d135ef5be101b88
mgbento <marcelobento21@gmail.com>
2020-03-04 19:56:11 -0300

    meu primeiro commit

---
+++ app/src/main/java/br/com/aula/projetociclodevidaexemplo/MainActivity.kt
@@ -0,0 +1,44 @@
+package br.com.aula.projetociclodevidaexemplo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.e("MainActivity", "onCreate()")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.e("MainActivity", "onStart()")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.e("MainActivity", "onResume()")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.e("MainActivity", "onPause()")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.e("MainActivity", "onStop()")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.e("MainActivity", "onRestart()")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e("MainActivity", "onDestroy()")
+    }
+}
